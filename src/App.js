a721453862d91934b59cf44a9a4877a5777c16f8
Olivia Culver <oculv21@gmail.com>
2019-12-10 11:11:39 -0500

    added react browser router, made App component a class component, made search component functional and passed down props to search component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import APIKey from './config.js';
 
@@ -6,15 +6,38 @@ import Search from './Search';
 import Nav from './Nav';
 import PhotoGallery from './PhotoGallery';
 
-function App() {
-  return (
-    <div className="container">
-      <Search />
-      <Nav />
-      <PhotoGallery />
-    </div>
-      
-  );
+class App extends Component {
+  state = {
+    searchQuery: ''
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.searchQuery);
+    this.setState({ searchQuery: ''});
+  }
+
+  handleInputChange = (e) => {
+    this.setState({ searchQuery: e.target.value });
+  }
+
+
+  render() {
+    return (
+      <div className="container">
+        <Search 
+          submit={this.handleSubmit}
+          query={this.state.searchQuery}
+          changeQuery={this.handleInputChange}
+        />
+        <Nav />
+        <PhotoGallery 
+          results={this.state.searchQuery}
+        />
+      </div>
+        
+    );
+  }
 }
 
 export default App;
